77eaaab4b1d62fabdeff8499906848e7a62130cf
KennethDyerHill <52476371+KennethDyerHill@users.noreply.github.com>
2020-05-26 20:30:19 -0400

    Database working

--- data/migrations/20200521191446_create_tickets.js
+++ data/migrations/20200521191446_create_tickets.js
@@ -1,8 +1,43 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable('tickets', tickets => {
+        tickets.increments();
   
-};
+        tickets
+          .string('category', 255)
+          .notNullable()
+        //   .unique();
+      tickets.string('description', 255).notNullable().unique();
+      tickets.string('tried', 255).notNullable();
+      tickets.string('comments', 255).notNullable();
+        //   tickets.integer('student_id').unique();
+        //   tickets.integer('helper_id').unique();
+        tickets.integer('student_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
 
-exports.down = function(knex) {
+        tickets.integer('helper_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+
+        tickets.string('urgency', 255).notNullable();
+
+        tickets.boolean('is_closed');
+
+        tickets.boolean('is_assigned');
+
+        tickets.timestamp('date_created');
+    });
+  };
   
-};
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('tickets');
+  };
